4b223e80b88704bc1e396f63da055c46d9e65dae
aaron <a.aa.akio.aaron@gmail.com>
2015-04-02 16:52:31 -0700

    transform - split runtime env into init and wrapper

--- build.js
+++ build.js
@@ -2,7 +2,22 @@ var fs = require('fs')
 var browserify = require('browserify')
 var uglify = require('uglify-stream');
 
-browserify('./lib/exec-wrapper.js')
-  .bundle()
-  .pipe(uglify())
-  .pipe(fs.createWriteStream('./build/exec-wrapper.js'))
+var files = [
+  'exec-init.js',
+  'exec-wrapper.js',
+]
+files.forEach(buildFile)
+
+
+function buildFile(filename){
+
+  var stream = browserify('./lib/'+filename).bundle()
+  
+  if (process.env.NODE_ENV !== 'dev') {
+    stream = stream.pipe(uglify())
+  }
+
+  stream = stream.pipe(fs.createWriteStream('./build/'+filename))
+  stream.on('finish', console.log.bind(console, 'vapor-proxy BUILD: bundled "'+filename+'".'))
+
+}
