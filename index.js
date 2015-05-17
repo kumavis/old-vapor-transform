d391ad60a4db2c8a9cf2816498d2dd1ec5d396b3
aaron <a.aa.akio.aaron@gmail.com>
2015-05-17 16:18:03 -0700

    vapor-transform initially working

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var PORT = process.env.PORT || 6000
+var PORT = process.env.PORT || 6001
 var express = require('express')
 var request = require('request')
 var cors = require('cors')
-var dappTransform = require('dapp-transform')
+var DappTransform = require('dapp-transform')
 
 var app = express()
 app.use(cors())
@@ -25,10 +25,13 @@ console.log('Vapor Dapp transform listening on', PORT)
 function getDapp(url) {
   var req = request({
     url: url,
-    followRedirects: false
+    // followRedirects: false
   })
   req.on('error', function(err) {
     console.error('BAD DAPP:', url, err)
   })
-  return req.pipe(dappTransform(url))
+  var dappTransform = DappTransform({
+    origin: url,
+  })
+  return req.pipe(dappTransform)
 }
