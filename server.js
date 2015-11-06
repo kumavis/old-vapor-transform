9ce5f8c38a0f060561c542e7fa22041ffed9c4a9
kumavis <aaron@kumavis.me>
2015-11-06 10:46:52 -0800

    cache - use multilevel or fallback to memdown

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const from = require('from')
 const streamToArray = require('stream-to-array')
+const eos = require('end-of-stream')
 const async = require('async')
 const express = require('express')
 const request = require('request')
 const cors = require('cors')
+const quickMulti = require('quick-multilevel')
+const levelup = require('levelup')
+const memdown = require('memdown')
 const hrtime = require('browser-process-hrtime')
 const prettyHrtime = require('pretty-hrtime')
 const HtmlTransform = require('./index.js').HtmlTransform
@@ -11,8 +15,8 @@ const JsTransform = require('./index.js').JsTransform
 const CssTransform = require('./index.js').CssTransform
 const generateEnvironment = require('./index.js').generateEnvironment
 const PORT = process.env.PORT || 9000
+const CACHE_DB = process.env.CACHE_DB
 
-var SIMPLE_CACHE = {}
 var envOpts = {}
 
 async.waterfall([
@@ -28,6 +32,8 @@ function startServer(environment, cb){
   var app = express()
   app.use(cors())
 
+  var cacheDb = createCacheDb()
+
   // transform html
   app.get('/html/:target', function(req, res) {
     var targetUrl = req.params.target
@@ -36,7 +42,7 @@ function startServer(environment, cb){
       targetUrl: targetUrl,
       environment: environment,
     })
-    performTransform('html', targetUrl, transform, res)
+    performTransform('html', targetUrl, cacheDb, transform, res)
   })
 
   // transform js
@@ -46,7 +52,7 @@ function startServer(environment, cb){
       targetUrl: targetUrl,
       environment: environment,
     })
-    performTransform('js', targetUrl, transform, res)
+    performTransform('js', targetUrl, cacheDb, transform, res)
   })
 
   // transform css
@@ -56,7 +62,7 @@ function startServer(environment, cb){
       targetUrl: targetUrl,
       environment: environment,
     })
-    performTransform('css', targetUrl, transform, res)
+    performTransform('css', targetUrl, cacheDb, transform, res)
   })
 
   // static assets
@@ -69,40 +75,55 @@ function startServer(environment, cb){
 
 // request target, perform tranform, respond
 // handles errors during this process
-function performTransform(label, url, transformStream, res){
+function performTransform(label, url, cacheDb, transformStream, res){
   console.log('transforming '+label+' => ' + url)
   var startTime = hrtime()
   var didAbort = false
   var req = null
   var cached = null
-  
-  // get request
-  try {
-    cached = SIMPLE_CACHE[url]
-    if (cached) {
-      req = from([cached])
-      req.on('error', onError)
-    } else {
-      req = request({ url: url })
-      req.on('error', onError)
-      req = req.pipe(transformStream)
-      req.on('error', onError)
-    }
-  } catch (err) {
-    onError(err)
-    return
+
+  tryCache(initiateTransform)
+
+  function tryCache(cb){
+    cacheDb.get(url, cb)
   }
 
-  streamToArray(req, onComplete)
+  function initiateTransform(_, cached){
+    // ignore err - means db read miss
 
-  req.pipe(res)
+    // get request
+    try {
+      if (cached) {
+        console.log('CACHE HIT =>', url)
+        req = from([cached])
+        req.on('error', onError)
+      } else {
+        console.log('CACHE MISS =>', url)
+        req = request({ url: url })
+        req.on('error', onError)
+        req = req.pipe(transformStream)
+        req.on('error', onError)
+        streamToArray(req, writeToCache)
+      }
+    } catch (err) {
+      return onError(err)
+    }
 
-  function onComplete(err, arr) {
+    eos(req, onComplete)
+    req.pipe(res)
+  }
+
+  function writeToCache(err, arr) {
     if (err) return
     if (didAbort) return
     // update cache
     var result = arr.join('')
-    SIMPLE_CACHE[url] = result
+    cacheDb.put(url, result)
+  }
+
+  function onComplete(err, arr) {
+    if (err) return
+    if (didAbort) return
     // completion message
     var totalTime = hrtime(startTime)
     var timeMessage = prettyHrtime(totalTime)
@@ -118,4 +139,16 @@ function performTransform(label, url, transformStream, res){
     var timeMessage = prettyHrtime(totalTime)
     console.log('transform interupted '+label+' ('+timeMessage+') => ' + url)
   }
+}
+
+function createCacheDb(){
+  var db = null
+  var label = 'transforms'
+  // use cache db when available, fallback to in-memory
+  if (CACHE_DB) {
+    db = quickMulti(CACHE_DB, label)
+  } else {
+    db = levelup(label, { db: memdown })
+  }
+  return db
 }
